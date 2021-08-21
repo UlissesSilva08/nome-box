f860d662ab9599cbf7c7c81ad9cc8dbc9b4f7422
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-20 23:25:30 -0300

    Inserindo a box com o nome no comando WriteLine

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nome_box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+---------+");
+            Console.WriteLine("| Ulisses |");
+            Console.WriteLine("+---------+");
+        }
+    }
+}
